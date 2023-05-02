59dc82a4adb1968e7dcde53dc3f6ee38201364e0
yty455 <yty455@naver.com>
2023-05-02 10:13:56 +0900

    feat[Controller]: add TripAreaController

--- src/main/java/com/example/enjoytrip/controller/TripAreaController.java
+++ src/main/java/com/example/enjoytrip/controller/TripAreaController.java
@@ -1,4 +1,4 @@
-package com.example.enjoytrip;
+package com.example.enjoytrip.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
