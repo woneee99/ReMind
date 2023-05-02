71ee89ee64acc4167aeb93fbd8182035f9bb42db
ksi2564 <kimsungin5@gmail.com>
2023-05-02 17:42:49 +0900

    feat[user]: login 화면 추가

--- src/main/java/com/example/enjoytrip/controller/LoginController.java
+++ src/main/java/com/example/enjoytrip/controller/LoginController.java
@@ -37,7 +37,6 @@ public class LoginController {
 
     @PutMapping
     public ResponseEntity<Integer> findPwd(@RequestBody Map<String, String> map){
-
         return ResponseEntity.ok().body(service.findPwd(map.get("userEmail")));
     }
 }
