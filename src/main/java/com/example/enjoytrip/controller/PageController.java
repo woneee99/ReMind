865e7e1c11a0b2daf72755ebfa709d7649f8e5fd
Inys <kimsungin5@gmail.com>
2023-05-06 22:07:08 +0900

    feat: 로그인, 여행지 검색 html 연결

--- src/main/java/com/example/enjoytrip/controller/PageController.java
+++ src/main/java/com/example/enjoytrip/controller/PageController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "sign-in.html";
+    }
+
     @GetMapping("/trip")
     public String getTripArea() {
         return "trip-area.html";
