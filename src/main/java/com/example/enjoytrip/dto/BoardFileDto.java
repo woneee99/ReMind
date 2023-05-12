f317375db6ccd4279ebceaa60892b74a9c73947b
Inys <kimsungin5@gmail.com>
2023-05-13 01:58:08 +0900

    feat[board]: 게시판 CRUD 구현(파일 첨부 가능)

---
+++ src/main/java/com/example/enjoytrip/dto/BoardFileDto.java
@@ -0,0 +1,20 @@
+package com.example.enjoytrip.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardFileDto {
+    private int fileId;
+    private int boardId;
+    private String fileName;
+    private long fileSize;
+    private String fileContentType;
+    private String fileUrl;
+    private LocalDateTime createdAt;
+}
