7b21f4e3c8c7abd41a4ebc05ba3105f94383b7bd
woneee99 <woneee99@gmail.com>
2023-05-16 17:55:00 +0900

    feat[login]: social login util & filter & handler

--- src/main/java/com/example/enjoytrip/oauth/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/example/enjoytrip/oauth/filter/TokenAuthenticationFilter.java
@@ -1,7 +1,12 @@
 package com.example.enjoytrip.oauth.filter;
 
+import com.example.enjoytrip.oauth.token.AuthToken;
+import com.example.enjoytrip.oauth.token.AuthTokenProvider;
+import com.example.enjoytrip.util.HeaderUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -13,8 +18,19 @@ import java.io.IOException;
 @Slf4j
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthTokenProvider tokenProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenStr = HeaderUtil.getAccessToken(request);
+        AuthToken token = tokenProvider.convertAuthToken(tokenStr);
 
+        if (token.validate()) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
