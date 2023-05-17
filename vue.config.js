07c612d6cad7ce6e0a91f28277f5d3f6032e3020
woneee99 <woneee99@gmail.com>
2023-05-17 17:47:52 +0900

    feat: backend 연결

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: "C:/Users/SSAFY/Documents/SpringBoot/EnjoyTrip/src/main/resources/static",
   transpileDependencies: true
 })
